deef354311c7d01b4284c79865649f5f45a05de3
Manuel98luna01~ <jmlzelaya22@gmail.com>
2022-02-11 14:37:26 -0600

    Login

---
+++ Recibe_get.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datos get</title>
+</head>
+<body>
+Nombre: <?php echo $_GET ['nombre'] ?><br>
+    Usuario: <?php echo $_GET ['usuario'] ?><br>
+    Password: <?php echo $_GET ['Password'] ?><br>
+    Password2: <?php echo $_GET ['Password2'] ?><br>
+    E-mail: <?php echo $_GET ['correo'] ?><br>
+</body>
+</html>
